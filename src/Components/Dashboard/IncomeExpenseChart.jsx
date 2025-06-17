14a43ca21be49a30df5d6a246e3ccd45633d05a9
nasir <nasiradan916@gmail.com>
2025-06-17 12:07:14 +0300

    Add

--- src/Components/Dashboard/IncomeExpenseChart.jsx
+++ src/Components/Dashboard/IncomeExpenseChart.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend } from 'recharts';
-import { useApp } from '../../context/AppContext';
+import { useApp } from '../../Context/AppContext';
 import { generateTimeSeriesData } from '../../utils';
 import Card from '../UI/Card';
 
